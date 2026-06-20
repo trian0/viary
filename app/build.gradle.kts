463b5add7250b74c57dc565391119904bad6f6ee
trian0 <thalesrian0@gmail.com>
2026-06-20 16:38:58 -0300

    build: 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.trian0.viary"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
